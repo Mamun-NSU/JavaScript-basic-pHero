83b1dfe134b73580797c18cafcaf714473b553d0
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-01-28 23:39:54 +0600

    basic variables

---
+++ basic.js
@@ -0,0 +1,8 @@
+// Declear Variablr
+let myName = "Abdullah Al Mamun";  // String
+let myAge = 26;   //number
+let myLanguage = ['C', 'HTML', 'CSS', 'JavaScript']; // array
+
+console.log(myName);
+console.log(myAge);
+console.log(myLanguage);
