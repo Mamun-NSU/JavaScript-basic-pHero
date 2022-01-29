f8de974e1ea82765939fc62c2b8989c746d87b34
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-01-29 22:55:18 +0600

    condition and object

--- basic.js
+++ basic.js
@@ -1,8 +1,27 @@
 // Declear Variablr
-let myName = "Abdullah Al Mamun";  // String
-let myAge = 26;   //number
-let myLanguage = ['C', 'HTML', 'CSS', 'JavaScript']; // array
+// let myName = "Abdullah Al Mamun";  // String
+// let myAge = 26;   //number
+// let myLanguage = ['C', 'HTML', 'CSS', 'JavaScript']; // array
 
-console.log(myName);
-console.log(myAge);
-console.log(myLanguage);
+// console.log(myName);
+// console.log(myAge);
+// console.log(myLanguage);
+
+// condition
+var myMoney = 120;
+var burgerPrice = 100;
+
+if (myMoney > burgerPrice) {
+    console.log("I will eat BURGER");
+} else
+    console.log("I haven't enough money");
+
+// object
+
+var myCar = {
+    color: "blue",
+    model: "BMW",
+    price: "3 crore",
+}
+console.log(myCar);
+
