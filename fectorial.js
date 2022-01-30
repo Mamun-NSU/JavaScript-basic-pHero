222686a2d6e386eb70c38ae84719cacb0812b2fa
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-01-30 18:54:01 +0600

    fectorial update

--- fectorial.js
+++ fectorial.js
@@ -1,4 +1,5 @@
 
+// Fectorial function
 function getFectorial(number){
     let fectorial = 1;
     for(var i = 1; i<= number; i++){
@@ -15,3 +16,6 @@ console.log("Fectorial of 10 is : ", secondFectorial);
 
 let thiredFectorial = getFectorial(5);
 console.log("Fectorial of 5 is : ", thiredFectorial);
+
+let forthFectorial = getFectorial(12);
+console.log("Fectorial of 12 is : ", forthFectorial);
