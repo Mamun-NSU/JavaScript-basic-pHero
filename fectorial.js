4f7827eab4f7e09f0ceaeef2fc8d5a0d83cbfc19
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-02-05 23:51:08 +0600

    condition applied

--- fectorial.js
+++ fectorial.js
@@ -1,8 +1,14 @@
 
 // Fectorial function
-function getFectorial(number){
+function getFectorial(number) {
+    if (typeof number != "number") {
+        return "Please enter a number";
+    }
+    if (number < 2) {
+        return "Please enter number bigger than 1";
+    }
     let fectorial = 1;
-    for(var i = 1; i<= number; i++){
+    for (var i = 1; i <= number; i++) {
         fectorial = fectorial * i;
     }
     return fectorial;
