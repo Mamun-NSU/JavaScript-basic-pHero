b2e8574f15fb3f16decb4215813e1c9b49d96560
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-02-07 13:26:13 +0600

    updates

--- bestFriend.js
+++ bestFriend.js
@@ -1,15 +1,20 @@
 
-function bestFriend(friends){
-   var myBestFriend = friends[0];
-    for(var i = 0; i<friends.length; i++){
+function bestFriend(friends) {
+    console.log(typeof friends)
+    if (typeof number != "object") {
+        return "Please enter a number";
+    }
+
+    var myBestFriend = friends[0];
+    for (var i = 0; i < friends.length; i++) {
         var friend = friends[i];
-        if(friend.length > myBestFriend.length){
+        if (friend.length > myBestFriend.length) {
             myBestFriend = friend;
         }
     }
     return myBestFriend;
 }
 
-var friends = ["Mamun", "Pias", "Misuk", "Sazzad","Abdullah Al Mamun", "Hasnat", "Sazzad", "Asifur", "Ibrahim", "Sujit"];
+var friends = ["Mamun", "Pias", "Misuk", "Sazzad", "Abdullah Al Mamun", "Hasnat", "Sazzad", "Asifur", "Ibrahim", "Sujit"];
 
 console.log("My best friend is: ", bestFriend(friends));
--- leapyear.js
+++ leapyear.js
@@ -1,15 +1,18 @@
 //Leap Year calculation
 
-function isLeapYear(year){
-    if(year%4==0){
-        if(year%100==0 && year%400 !=0){
+function isLeapYear(year) {
+    if (typeof number != "number") {
+        return "Please enter a number";
+    }
+    if (year % 4 == 0) {
+        if (year % 100 == 0 && year % 400 != 0) {
             console.log(year, "is not leapyear");
-           return;
+            return;
         }
         console.log(year, "is leapyear");
     }
     else
-    console.log(year, "is not leapyear");
+        console.log(year, "is not leapyear");
 }
 
 isLeapYear(1900);
