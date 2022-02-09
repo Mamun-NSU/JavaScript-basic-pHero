85e1d97ee6c971db986126c4dddcd9fadbddc4ef
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-02-09 23:37:46 +0600

    update

--- feetToInch.js
+++ feetToInch.js
@@ -3,6 +3,9 @@ function feetToInch(feet) {
     if (typeof feet != "number") {
         return "Please enter a number";
     }
+    if (typeof feet < 0) {
+        return "Please enter positive number";
+    }
     var inch = feet * 12;
     return inch;
 }
