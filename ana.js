ba949c8d5458cc9711ba7c85ca944f168dbaa449
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-02-18 23:58:15 +0600

    ana to vori calculation function

---
+++ ana.js
@@ -0,0 +1,16 @@
+function anaToVori(ana) {
+    // IF the parameter of this function is not number, then this error message shown
+    if (typeof ana != 'number') {
+        return 'Please give a munber';
+    }
+    // IF the amount number of ana is negavive [less than 0], then this error message shown
+    if (ana < 0) {
+        return 'Please give a positive value';
+    }
+    let vori;
+    vori = ana / 16;
+    return vori;
+}
+
+let testAna1 = 32;
+console.log(anaToVori(testAna1));
