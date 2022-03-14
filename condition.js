644905def6ad59efec6b6fd52ef80263dced081d
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-03-14 23:48:45 +0600

    conditions

---
+++ condition.js
@@ -0,0 +1,23 @@
+
+const marks = 84;
+if (marks > 80) {
+    console.log("Gread: A+");
+}
+else if (marks > 70) {
+    console.log("Gread: A");
+}
+else if (marks > 60) {
+    console.log("Gread: A-");
+}
+else if (marks > 50) {
+    console.log("Gread: B");
+}
+else if (marks > 40) {
+    console.log("Gread: C");
+}
+else if (marks > 33) {
+    console.log("Gread: D");
+}
+else {
+    console.log("Failed");
+}
